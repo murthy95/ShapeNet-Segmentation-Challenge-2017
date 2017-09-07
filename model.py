eadb5479d499209f1addebbcf017587878028b74
jlnmurthy95 <jlnmurthy95@gmail.com>
2017-09-07 23:58:03 +0530

    Added BatchNorm

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ from keras import backend as keras
 from keras.metrics import categorical_accuracy
 from keras.activations import softmax
 from keras import backend as K
+from keras.layers.normalization import BatchNormalization
 
 
 class myUnet(object):
@@ -55,47 +56,67 @@ class myUnet(object):
 		inputs_mirrored = merge([up_crop,inputs_mirrored], mode = 'concat', concat_axis = 1)
 		print "inputs shape:",inputs_mirrored.shape
 
-		conv1 = Conv2D(64, (3,3), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(inputs_mirrored)
+		conv1 = Conv2D(64, (3,3), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(inputs_mirrored)
 		print "conv1 shape:",conv1.shape
-		conv1 = Conv2D(64, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(conv1)
+		conv1 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv1)
+		conv1 = Activation('relu')(conv1)
+		conv1 = Conv2D(64, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(conv1)
 		print "conv1 shape:",conv1.shape
+		conv1 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv1)
+		conv1 = Activation('relu')(conv1)
 		crop1 = Cropping2D(cropping=((184,184),(0,0)))(conv1)
 		print "crop1 shape:",crop1.shape
 		pool1 = AveragePooling2D(pool_size=(2, 1))(conv1)
 		print "pool1 shape:",pool1.shape
 
-		conv2 = Conv2D(128, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(pool1)
+		conv2 = Conv2D(128, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(pool1)
 		print "conv2 shape:",conv2.shape
-		conv2 = Conv2D(128, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(conv2)
+		conv2 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv2)
+		conv2 = Activation('relu')(conv2)
+		conv2 = Conv2D(128, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(conv2)
 		print "conv2 shape:",conv2.shape
+		conv2 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv2)
+		conv2 = Activation('relu')(conv2)
 		crop2 = Cropping2D(cropping=((88,88),(0,0)))(conv2)
 		print "crop2 shape:",crop2.shape
 		pool2 = MaxPooling2D(pool_size=(2,1))(conv2)
 		print "pool2 shape:",pool2.shape
 
-		conv3 = Conv2D(256, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(pool2)
+		conv3 = Conv2D(256, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(pool2)
 		print "conv3 shape:",conv3.shape
-		conv3 = Conv2D(256, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(conv3)
+		conv3 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv3)
+		conv3 = Activation('relu')(conv3)
+		conv3 = Conv2D(256, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(conv3)
 		print "conv3 shape:",conv3.shape
+		conv3 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv3)
+		conv3 = Activation('relu')(conv3)
 		crop3 = Cropping2D(cropping=((40,40),(0,0)))(conv3)
 		print "crop3 shape:",crop3.shape
 		pool3 = MaxPooling2D(pool_size=(2,1))(conv3)
 		print "pool3 shape:",pool3.shape
 
-		conv4 = Conv2D(512, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(pool3)
+		conv4 = Conv2D(512, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(pool3)
 		print "conv4 shape:",conv4.shape
-		conv4 = Conv2D(512, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(conv4)
+		conv4 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv4)
+		conv4 = Activation('relu')(conv4)
+		conv4 = Conv2D(512, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(conv4)
 		print "conv4 shape:",conv4.shape
+		conv4 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv4)
+		conv4 = Activation('relu')(conv4)
 		drop4 = Dropout(0.5)(conv4)
 		crop4 = Cropping2D(cropping=((16,16),(0,0)))(drop4)
 		print "crop4 shape:",crop4.shape
 		pool4 = MaxPooling2D(pool_size=(2,1))(drop4)
 		print "pool4 shape:",pool4.shape
 
-		conv5 = Conv2D(1024, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(pool4)
+		conv5 = Conv2D(1024, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(pool4)
 		print "conv5 shape:",conv5.shape
-		conv5 = Conv2D(1024, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(conv5)
+		conv5 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv5)
+		conv5 = Activation('relu')(conv5)
+		conv5 = Conv2D(1024, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(conv5)
 		print "conv5 shape:",conv5.shape
+		conv5 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv5)
+		conv5 = Activation('relu')(conv5)
 		drop5 = Dropout(0.5)(conv5)
 		crop5 = Cropping2D(cropping=((4,4),(0,0)))(drop5)
 		print "crop5 shape:",crop5.shape
@@ -103,10 +124,14 @@ class myUnet(object):
 		print "pool5 shape:",pool5.shape
 
 
-		conv6 = Conv2D(2048, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(pool5)
+		conv6 = Conv2D(2048, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(pool5)
 		print "conv6 kerasshape:",conv6.shape
-		conv6 = Conv2D(2048, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(conv6)
+		conv6 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv6)
+		conv6 = Activation('relu')(conv6)
+		conv6 = Conv2D(2048, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(conv6)
 		print "conv6 shape:",conv6.shape
+		conv6 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv6)
+		conv6 = Activation('relu')(conv6)
 		# conv6 = Conv2D(2048, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(conv6)
 		# print "conv6 shape:",conv6.shape
 		drop6 = Dropout(0.5)(conv6)
@@ -116,37 +141,53 @@ class myUnet(object):
 		print "up7 shape:",up7.shape
 		merge7 = merge([crop5,up7], mode = 'concat', concat_axis = 3)
 		print "merge7 shape:",merge7.shape
-		conv7 = Conv2D(1024, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(merge7)
+		conv7 = Conv2D(1024, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(merge7)
 		print "conv7 shape:",conv7.shape
-		conv7 = Conv2D(1024, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(conv7)
+		conv7 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv7)
+		conv7 = Activation('relu')(conv7)
+		conv7 = Conv2D(1024, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(conv7)
 		print "conv7 shape:",conv7.shape
+		conv7 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv7)
+		conv7 = Activation('relu')(conv7)
 
 		up8 = Conv2D(512, (1,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(UpSampling2D(size = (2,1))(conv7))
 		print "up8 shape:",up8.shape
 		merge8 = merge([crop4,up8], mode = 'concat', concat_axis = 3)
 		print "merge8 shape:",merge8.shape
-		conv8 = Conv2D(512, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(merge8)
+		conv8 = Conv2D(512, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(merge8)
 		print "conv8 shape:",conv8.shape
-		conv8 = Conv2D(512, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(conv8)
+		conv8 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv8)
+		conv8 = Activation('relu')(conv8)
+		conv8 = Conv2D(512, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(conv8)
 		print "conv8 shape:",conv8.shape
+		conv8 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv8)
+		conv8 = Activation('relu')(conv8)
 
 		up9 = Conv2D(256, (1,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(UpSampling2D(size = (2,1))(conv8))
 		print "up9 shape:",up9.shape
 		merge9 = merge([crop3,up9], mode = 'concat', concat_axis = 3)
 		print "merge9 shape:",merge9.shape
-		conv9 = Conv2D(256, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(merge9)
+		conv9 = Conv2D(256, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(merge9)
 		print "merge9 shape:",merge9.shape
-		conv9 = Conv2D(256, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(conv9)
+		conv9 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv9)
+		conv9 = Activation('relu')(conv9)
+		conv9 = Conv2D(256, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(conv9)
 		print "merge9 shape:",merge9.shape
+		conv9 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv9)
+		conv9 = Activation('relu')(conv9)
 
 		up10 = Conv2D(128, (1,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(UpSampling2D(size = (2,1))(conv9))
 		print "up10 shape:",up10.shape
 		merge10 = merge([crop2,up10], mode = 'concat', concat_axis = 3)
 		print "merge10 shape:",merge10.shape
-		conv10 = Conv2D(128, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(merge10)
+		conv10 = Conv2D(128, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(merge10)
 		print "conv10 shape:",conv10.shape
-		conv10 = Conv2D(128, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(conv10)
+		conv10 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv10)
+		conv10 = Activation('relu')(conv10)
+		conv10 = Conv2D(128, (3,1), activation = 'linear', padding = 'valid', kernel_initializer = 'glorot_normal')(conv10)
 		print "conv10 shape:",conv10.shape
+		conv10 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv10)
+		conv10 = Activation('relu')(conv10)
 
 		up11 = Conv2D(64, (1,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(UpSampling2D(size = (2,1))(conv10))
 		print "up11 shape:",up11.shape
@@ -154,11 +195,18 @@ class myUnet(object):
 		print "merge11 shape:",merge11.shape
 		conv11 = Conv2D(64, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(merge11)
 		print "conv11 shape:",conv11.shape
+		conv11 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv11)
+		conv11 = Activation('relu')(conv11)
 		conv11 = Conv2D(32,(3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(conv11)
 		print "conv11 shape:",conv11.shape
+		conv11 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv11)
+		conv11 = Activation('relu')(conv11)
 		conv11 = Conv2D(16, (3,1), activation = 'relu', padding = 'valid', kernel_initializer = 'glorot_normal')(conv11)
 		print "conv11 shape:",conv11.shape
+		conv11 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv11)
+		conv11 = Activation('relu')(conv11)
 		conv11 = Conv2D(6, (3,1), padding = 'valid', kernel_initializer = 'glorot_normal')(conv11)
+		conv11 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(conv11)
 		print "conv11 shape:",conv11.shape
 		conv11 = Reshape((2048, 6))(conv11)
 		print "conv11 shape:",conv11.shape
@@ -181,12 +229,12 @@ class myUnet(object):
 		x_train, y_train, x_val, y_val = self.load_data()
 		print("loading data done")
 		model = self.get_unet()
-		model.load_weights('unet.hdf5')
+		# model.load_weights('unet.hdf5')
 		print("got unet")
 
 		model_checkpoint = ModelCheckpoint('unet.hdf5', monitor='loss',verbose=1, save_best_only=True)
 		print('Fitting model...')
-		model.fit(x_train, y_train, batch_size=32, epochs=50, verbose=1, shuffle=True, callbacks=[model_checkpoint])
+		model.fit(x_train, y_train, batch_size=2, epochs=50, verbose=1, shuffle=True, callbacks=[model_checkpoint])
 
 		print('predict test data')
 		val_score = model.evaluate(x_val,y_val, batch_size=52, verbose=1)
